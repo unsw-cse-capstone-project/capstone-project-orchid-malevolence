edb27d0611ff3c72578e71616fb435f8c26054f4
YY-GAO <littlelamb0000@gmail.com>
2020-07-13 14:59:06 +0800

    change homepage layout

---
+++ website/mainpage/src/components/homepage_components/homepage_goal.vue
@@ -0,0 +1,41 @@
+<template>
+    <div class="goal" id="app_goal">
+        {{ goal_title }}
+    </div>
+
+</template>
+
+<script>
+
+    const now = new Date()
+    const nowMonth = now.getMonth() + 1
+    const nowYear = now.getFullYear()
+
+    export default {
+        name: 'homepage-goal',
+
+        data() {
+            return {
+                goal_title: nowYear + "-" + nowMonth + " Reading Challenge"
+            }
+        }
+    }
+
+
+
+</script>
+
+<style lang="less" scoped>
+    .goal {
+        margin: auto;
+        height: 200px;
+        width: 200px;
+        border-radius: 2px;
+        background: crimson;
+        padding-top: 30px;
+    }
+</style>
+
+
+
+
