d34a85d6b6d664f14c5f6f3ec4af024ab093af38
ChangLu Ma <changluma5@gmail.com>
2020-08-04 17:06:00 +1000

    Updating

--- website/mainpage/src/pages/login.vue
+++ website/mainpage/src/pages/login.vue
@@ -10,6 +10,7 @@
 			<div class="register_title_wrap">
 				<div class="register_title">Login</div>
 			</div>
+			<div class="register_form">
 			<!-- Username -->
 			<el-form-item label="Username" prop="username">
 				<el-input v-model="loginForm.username" placeholder="username" prefix-icon="iconfont icon-user"></el-input>
@@ -22,6 +23,7 @@
 			<el-form-item class="btns">
 				<el-button type="primary" @click="login">Login</el-button>
 			</el-form-item>
+			</div>
 			<div class="other">
 				No account number? <router-link to='/register'>register</router-link>
 			</div>
@@ -76,7 +78,7 @@ methods: {
 }
 </script>
 
-<style lang="less" scoped>
+<style lang="less">
 body, html{
 	height: 100%;
 	overflow: hidden;
@@ -85,7 +87,7 @@ body, html{
 	position: absolute;
 	height: 100%;
 	width: 100%;
-	background: url(../assets/background_login.jpg) no-repeat fixed;
+	background: url(../assets/login_img.jpg) no-repeat fixed;
 	background-size: cover;
 	background-origin: border-box;
 }
@@ -107,26 +109,36 @@ body, html{
 	right: 100px;
 	width: 590px;
 	height: 586px;
-	background-image: repeating-linear-gradient(135deg,rgba(0,0,0,0.05),rgba(225,169,131,.5));
+	background-image: repeating-linear-gradient(135deg,rgba(0,0,0,.05),rgba(43,44,46,.5));
 	border-radius: 30px;
 }
 
 // title Center + font
 .register_title{
 	text-align: center;
-	font: 34px/168px bolder sans-serif;
+	font: 50px/168px bolder sans-serif;
+	font-style: italic;
+	color: white;
 }
 // Processing of each single column table
 .el-form-item{
 	height: 62.5px;
-	margin-left: 30px;
+	margin-left: 80px;
 }
+.el-form-item__label{
+	color: white;
+	font-size: 17px;
+	margin-right: 10px;
+}
+
 // Modify the length of the input field
+
 .el-input{
 	width: 300px;
 }
 .other{
 	text-align: center;
 	font: bolder;
+	color: white;
 }
 </style>
